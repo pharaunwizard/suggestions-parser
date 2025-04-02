4d476b5b8c6ee6b5b3e736d3b6297f1f0cb59102
pharaunwizard <pharaunwizard@gmail.com>
2025-04-02 17:49:09 +0500

    added proxy class

---
+++ SuggestionsParser/Proxy.cs
@@ -0,0 +1,32 @@
+﻿namespace SuggestionsParser;
+
+public record Proxy
+{
+    public Uri Uri { get; }
+    public string Username { get; }
+    public string Password { get; }
+
+    private Proxy(string proxy) : this(new Uri($"socks5://{proxy}")) { }
+
+    private Proxy(Uri uri)
+    {
+        Uri = uri ?? throw new ArgumentException("uri is null");
+
+        if (string.IsNullOrEmpty(uri.UserInfo))
+            throw new ArgumentException($"Invalid proxy format: {uri}");
+
+        var userInfo = uri.UserInfo.Split(':');
+        if (userInfo.Length != 2)
+            throw new ArgumentException($"Invalid proxy format: {uri}");
+
+        Username = userInfo[0];
+        Password = userInfo[1];
+    }
+
+    public static async Task<List<Proxy>> FromFileAsync(string path)
+    {
+        var lines = await File.ReadAllLinesAsync(path);
+        return lines.Select(line => new Proxy(line)).ToList();
+    }
+}
+
