0c98aa73d188714dd38bf67330ae3111ddc2d353
pharaunwizard <pharaunwizard@gmail.com>
2025-04-02 17:50:42 +0500

    added utils class

---
+++ SuggestionsParser/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace SuggestionsParser;
+
+public static class Utils
+{
+    public static HttpClient CreateHttpClient(Proxy proxy)
+    {
+        var webProxy = new WebProxy
+        {
+            Address = proxy.Uri,
+            Credentials = new NetworkCredential(proxy.Username, proxy.Password)
+        };
+
+        var handler = new SocketsHttpHandler()
+        {
+            UseProxy = true,
+            Proxy = webProxy,
+        };
+
+        return new HttpClient(handler, disposeHandler: true);
+    }
+}
+
