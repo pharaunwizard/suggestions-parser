affdaa02c1f4764eec8e89dc3522e95966bbf699
pharaunwizard <pharaunwizard@gmail.com>
2025-04-02 17:47:37 +0500

    added config class

---
+++ SuggestionsParser/Config.cs
@@ -0,0 +1,12 @@
+﻿namespace SuggestionsParser;
+
+public static class Config
+{
+    public const string ProxiesFile = "proxies.txt";
+    public const string InputKeysFile = "keys.txt";
+    public const string OutputKeysFile = "output_keys.txt";
+
+    public const string UserInterfaceLanguage = "en";
+    public const string Symbols = "abcdefghijklmnopqrstuvwxyz";
+}
+
